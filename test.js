eda9a09126e6354207a2d5c04c3f66bb56b19db8
sneoldum <neoldums@gmail.com>
2023-03-30 12:48:39 +0300

    Js update
    Js file now works

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
+
 function redirecting(element) {
     var value = element.value;
-    if (value == "Home page") {
-        alert("Redirecting to home page!");
-        window.location.href = "./index.html";
-    } else if (value == "About") {
-        alert("Redirecting to about page!");
-        window.location.href = "./about.html";
-    } else if (value == "Contact") {
-        alert("Redirecting to contact page!");
-        window.location.href = "./contact.html";
+    console.log(value);
+    if (value) {
+        alert("Redirecting to "+ value + " page!");
+        if (value == "Home") {
+            window.location.href = "./index.html";
+        }else{
+        window.location.href = "./"+value+".html";
+        }
     }
 }
 
