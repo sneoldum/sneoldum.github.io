d5478d9939987442aa00a188a829da46540bf8f6
sneoldum <neoldums@gmail.com>
2023-04-06 10:48:43 +0300

    js fault solved

--- site.js
+++ site.js
@@ -1,1 +1,15 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function redirecting(element) {
+    var value = element.value;
+    console.log(value);
+    if (value) {
+        value = value.toLowerCase();
+        alert("Redirecting to "+ value + " page!");
+        if (value == "home") {
+            window.location.href = "./index.html";
+        }else{
+        window.location.href = "./"+value+".html";
+        }
+    }
+}
